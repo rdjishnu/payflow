811eadab7343b75659262f34dddf87f10d8e63c9
R D JISHNU PRIYAN <jishnupriyan2007@gmail.com>
2026-08-18 23:33:08 +0530

    feat: configure Swagger OpenAPI to accept JWT Bearer authentication

--- src/main/java/com/payflow/payflow/PayflowApplication.java
+++ src/main/java/com/payflow/payflow/PayflowApplication.java
@@ -2,12 +2,21 @@ package com.payflow.payflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@SecurityScheme(
+    name = "Bearer Authentication",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
 @SpringBootApplication
-@EnableCaching
 public class PayflowApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PayflowApplication.class, args);
     }
+
 }
